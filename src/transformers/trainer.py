b7fd88e566e0355f76531bb4ec0a294d9d8025d2
Robert Myers <robertmyers@hey.com>
2020-08-06 18:25:20 -0500

    added for data generator

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -16,6 +16,7 @@ from torch.utils.data.dataloader import DataLoader
 from torch.utils.data.dataset import Dataset
 from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data.sampler import RandomSampler, Sampler, SequentialSampler
+from torch.utils.data.dataset import IterableDataset
 from tqdm.auto import tqdm, trange
 
 from .data.data_collator import DataCollator, default_data_collator
@@ -237,25 +238,23 @@ class Trainer:
             )
 
     def get_train_dataloader(self) -> DataLoader:
-        """
-        Returns the training :class:`~torch.utils.data.DataLoader`.
-
-        Will use no sampler if :obj:`self.train_dataset` is a :obj:`torch.utils.data.IterableDataset`, a random sampler
-        (adapted to distributed training if necessary) otherwise.
-
-        Subclass and override this method if you want to inject some custom behavior.
-        """
         if self.train_dataset is None:
             raise ValueError("Trainer: training requires a train_dataset.")
-        train_sampler = self._get_train_sampler()
-
-        return DataLoader(
+        if is_torch_tpu_available():
+            train_sampler = get_tpu_sampler(self.train_dataset)
+        else:
+            train_sampler = (
+                RandomSampler(self.train_dataset)
+                if self.args.local_rank == -1
+                else DistributedSampler(self.train_dataset)
+            )
+        data_loader = DataLoader(
             self.train_dataset,
             batch_size=self.args.train_batch_size,
-            sampler=train_sampler,
+            sampler=train_sampler if not isinstance(self.train_dataset, IterableDataset) else None,
             collate_fn=self.data_collator,
-            drop_last=self.args.dataloader_drop_last,
         )
+        return data_loader
 
     def _get_eval_sampler(self, eval_dataset: Dataset) -> Optional[torch.utils.data.sampler.Sampler]:
         if isinstance(eval_dataset, torch.utils.data.IterableDataset):
@@ -732,7 +731,6 @@ class Trainer:
 
         model.train()
         inputs = self._prepare_inputs(inputs, model)
-
         if self.args.fp16 and _use_native_amp:
             with autocast():
                 outputs = model(**inputs)
